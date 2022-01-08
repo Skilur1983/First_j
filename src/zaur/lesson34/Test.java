2f3729778d632a28359ea7cd733b6ca0aa5e7226
skilur1983 <skilur2009@gmail.com>
2022-01-08 09:17:25 +0200

    - updating Test class with lesson 35; - adding example of Predicate usage for ArrayList; - some ArrayList functions with Predicate;

--- src/zaur/lesson34/Test.java
+++ src/zaur/lesson34/Test.java
@@ -1,5 +1,8 @@
 package zaur.lesson34;
 
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
 public class Test {
     static void def(TestCount count) {
         System.out.println(count.abc("Hello", "Bye"));
@@ -13,6 +16,22 @@ public class Test {
         /* one can use several params and
          * several params update in method,
          * but there should be only one return statement */
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(3);
+        numbers.add(5);
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(7);
+        Predicate<Integer> p = x -> x % 3 == 0;
+        numbers.removeIf(p);
+        /* an equivalent of:
+        numbers.removeIf(x -> x % 3 ==0); */
+        System.out.println(numbers);
+        numbers.sort((x, y) -> -x.compareTo(y)); // "-" - sorts in desc
+        System.out.println(numbers);
     }
 }
 
